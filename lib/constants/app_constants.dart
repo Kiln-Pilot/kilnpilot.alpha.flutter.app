33ad76b7c7b13ca2eb7156db1c058e80364822bc
nailsonseat <aadarshv3@gmail.com>
2025-09-25 01:56:09 +0530

    update base url structure

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 
 class AppConstants {
   static const String appName = "Kiln Pilot";
-  static const bool isSsl = true;
-  static const String host = "api.myflutterapp.com";
-  static const String baseUrl = "${isSsl ? "https" : "http"}://$host";
+  static const bool isSsl = false;
+  static const bool mentionPort = true;
+  static const String host = "localhost";
+  static const int port = 8000;
+  static const String apiPath = "/api/v1";
+  static const String baseUrl =
+      "${isSsl ? "https" : "http"}://$host:"
+      "${mentionPort ? port : ""}"
+      "$apiPath";
   static const Color seedColor = Colors.deepPurple;
 }
