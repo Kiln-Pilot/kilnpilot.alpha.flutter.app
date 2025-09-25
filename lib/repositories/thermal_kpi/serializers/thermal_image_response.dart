ff1102c2507208d4d877fe8325c41a998f4a13b2
nailsonseat <aadarshv3@gmail.com>
2025-09-25 22:17:17 +0530

    update serializers

--- lib/repositories/thermal_kpi/serializers/thermal_image_response.dart
+++ lib/repositories/thermal_kpi/serializers/thermal_image_response.dart
@@ -6,14 +6,16 @@ part 'thermal_image_response.g.dart';
 class ThermalImageResponse {
   final String status;
   final String? error;
-  final ThermalAnalysis? analysis;
-  final String? annotatedImageUrl;
+  final ImageInfo? imageInfo;
+  final ThermalImageAnalysis? analysis;
+  final String? annotatedImage;
 
   ThermalImageResponse({
     required this.status,
     this.error,
+    this.imageInfo,
     this.analysis,
-    this.annotatedImageUrl,
+    this.annotatedImage,
   });
 
   factory ThermalImageResponse.fromJson(Map<String, dynamic> json) => _$ThermalImageResponseFromJson(json);
@@ -21,30 +23,77 @@ class ThermalImageResponse {
 }
 
 @JsonSerializable()
-class ThermalAnalysis {
-  final int totalDetections;
-  final List<ThermalRegion> regions;
+class ImageInfo {
+  final int width;
+  final int height;
+  final int channels;
 
-  ThermalAnalysis({
-    required this.totalDetections,
-    required this.regions,
+  ImageInfo({
+    required this.width,
+    required this.height,
+    required this.channels,
   });
 
-  factory ThermalAnalysis.fromJson(Map<String, dynamic> json) => _$ThermalAnalysisFromJson(json);
-  Map<String, dynamic> toJson() => _$ThermalAnalysisToJson(this);
+  factory ImageInfo.fromJson(Map<String, dynamic> json) => _$ImageInfoFromJson(json);
+  Map<String, dynamic> toJson() => _$ImageInfoToJson(this);
 }
 
 @JsonSerializable()
-class ThermalRegion {
-  final double temperature;
-  final List<int> boundingBox;
+class ThermalImageAnalysis {
+  final int totalDetections;
+  final int highTemperatureCount;
+  final double maxTemperature;
+  final double minTemperature;
+  final List<ThermalDetection> detections;
 
-  ThermalRegion({
-    required this.temperature,
-    required this.boundingBox,
+  ThermalImageAnalysis({
+    required this.totalDetections,
+    required this.highTemperatureCount,
+    required this.maxTemperature,
+    required this.minTemperature,
+    required this.detections,
   });
 
-  factory ThermalRegion.fromJson(Map<String, dynamic> json) => _$ThermalRegionFromJson(json);
-  Map<String, dynamic> toJson() => _$ThermalRegionToJson(this);
+  factory ThermalImageAnalysis.fromJson(Map<String, dynamic> json) => _$ThermalImageAnalysisFromJson(json);
+  Map<String, dynamic> toJson() => _$ThermalImageAnalysisToJson(this);
 }
 
+@JsonSerializable()
+class ThermalDetection {
+  final int detectionId;
+  final BoundingBox boundingBox;
+  final double temperature;
+  final bool isHighTemperature;
+  final int area;
+  final int contourArea;
+
+  ThermalDetection({
+    required this.detectionId,
+    required this.boundingBox,
+    required this.temperature,
+    required this.isHighTemperature,
+    required this.area,
+    required this.contourArea,
+  });
+
+  factory ThermalDetection.fromJson(Map<String, dynamic> json) => _$ThermalDetectionFromJson(json);
+  Map<String, dynamic> toJson() => _$ThermalDetectionToJson(this);
+}
+
+@JsonSerializable()
+class BoundingBox {
+  final int x;
+  final int y;
+  final int width;
+  final int height;
+
+  BoundingBox({
+    required this.x,
+    required this.y,
+    required this.width,
+    required this.height,
+  });
+
+  factory BoundingBox.fromJson(Map<String, dynamic> json) => _$BoundingBoxFromJson(json);
+  Map<String, dynamic> toJson() => _$BoundingBoxToJson(this);
+}
--- lib/repositories/thermal_kpi/serializers/thermal_video_response.dart
+++ lib/repositories/thermal_kpi/serializers/thermal_video_response.dart
@@ -6,11 +6,13 @@ part 'thermal_video_response.g.dart';
 class ThermalVideoResponse {
   final String status;
   final String? error;
+  final VideoInfo? videoInfo;
   final VideoAnalysis? analysis;
 
   ThermalVideoResponse({
     required this.status,
     this.error,
+    this.videoInfo,
     this.analysis,
   });
 
@@ -19,10 +21,38 @@ class ThermalVideoResponse {
 }
 
 @JsonSerializable()
+class VideoInfo {
+  final int width;
+  final int height;
+  final double fps;
+  final int totalFrames;
+  final double duration;
+
+  VideoInfo({
+    required this.width,
+    required this.height,
+    required this.fps,
+    required this.totalFrames,
+    required this.duration,
+  });
+
+  factory VideoInfo.fromJson(Map<String, dynamic> json) => _$VideoInfoFromJson(json);
+  Map<String, dynamic> toJson() => _$VideoInfoToJson(this);
+}
+
+@JsonSerializable()
 class VideoAnalysis {
+  final int totalDetections;
+  final int totalHighTemperatureDetections;
+  final double maxTemperature;
+  final double minTemperature;
   final List<FrameAnalysis> frames;
 
   VideoAnalysis({
+    required this.totalDetections,
+    required this.totalHighTemperatureDetections,
+    required this.maxTemperature,
+    required this.minTemperature,
     required this.frames,
   });
 
@@ -33,11 +63,17 @@ class VideoAnalysis {
 @JsonSerializable()
 class FrameAnalysis {
   final int frameNumber;
-  final List<ThermalRegion> regions;
+  final double timestamp;
+  final List<ThermalDetection> detections;
+  final int highTemperatureCount;
+  final double maxTemperature;
 
   FrameAnalysis({
     required this.frameNumber,
-    required this.regions,
+    required this.timestamp,
+    required this.detections,
+    required this.highTemperatureCount,
+    required this.maxTemperature,
   });
 
   factory FrameAnalysis.fromJson(Map<String, dynamic> json) => _$FrameAnalysisFromJson(json);
@@ -45,16 +81,41 @@ class FrameAnalysis {
 }
 
 @JsonSerializable()
-class ThermalRegion {
+class ThermalDetection {
+  final int detectionId;
+  final BoundingBox boundingBox;
   final double temperature;
-  final List<int> boundingBox;
+  final bool isHighTemperature;
+  final int area;
+  final int contourArea;
 
-  ThermalRegion({
-    required this.temperature,
+  ThermalDetection({
+    required this.detectionId,
     required this.boundingBox,
+    required this.temperature,
+    required this.isHighTemperature,
+    required this.area,
+    required this.contourArea,
   });
 
-  factory ThermalRegion.fromJson(Map<String, dynamic> json) => _$ThermalRegionFromJson(json);
-  Map<String, dynamic> toJson() => _$ThermalRegionToJson(this);
+  factory ThermalDetection.fromJson(Map<String, dynamic> json) => _$ThermalDetectionFromJson(json);
+  Map<String, dynamic> toJson() => _$ThermalDetectionToJson(this);
 }
 
+@JsonSerializable()
+class BoundingBox {
+  final int x;
+  final int y;
+  final int width;
+  final int height;
+
+  BoundingBox({
+    required this.x,
+    required this.y,
+    required this.width,
+    required this.height,
+  });
+
+  factory BoundingBox.fromJson(Map<String, dynamic> json) => _$BoundingBoxFromJson(json);
+  Map<String, dynamic> toJson() => _$BoundingBoxToJson(this);
+}
