dcfcb44c811d9d38539521f3ddcebe9df28d1850
nailsonseat <aadarshv3@gmail.com>
2025-09-27 01:05:41 +0530

    fix typho

--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:kilnpilot_alpha_flutter_app/blocs/chatbot/chatbot_bloc.dart';
+import 'package:kilnpilot_alpha_flutter_app/blocs/chatbot/chatbot_session_bloc.dart';
 import 'package:kilnpilot_alpha_flutter_app/repositories/thermal_kpi/thermal_kpi_repository.dart';
 import 'package:kilnpilot_alpha_flutter_app/screens/dashboard/dashboard.dart';
 import 'package:kilnpilot_alpha_flutter_app/screens/dashboard/dashboard_shell.dart';
-import 'package:kilnpilot_alpha_flutter_app/screens/kiln_tempreature/kiln_temperature_screen.dart';
+import 'package:kilnpilot_alpha_flutter_app/screens/kiln_temperature/kiln_temperature_screen.dart';
 import 'package:kilnpilot_alpha_flutter_app/screens/optimization/optimization_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kilnpilot_alpha_flutter_app/blocs/thermal_kpi/thermal_kpi_bloc.dart';
+import 'package:kilnpilot_alpha_flutter_app/screens/chat/chatbot_screen.dart';
+import 'package:kilnpilot_alpha_flutter_app/repositories/cement_operations/cement_operations_repository.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -29,7 +33,19 @@ final GoRouter router = GoRouter(
         ),
         GoRoute(
           path: '/dashboard/chat',
-          pageBuilder: (context, state) => MaterialPage(child: ChatScreen()),
+          pageBuilder: (context, state) => MaterialPage(
+            child: MultiBlocProvider(
+              providers: [
+                BlocProvider(
+                  create: (_) => ChatbotBloc(CementOperationsRepository()),
+                ),
+                BlocProvider(
+                  create: (_) => ChatbotSessionBloc(CementOperationsRepository())
+                ),
+              ],
+              child: ChatbotScreen(),
+            ),
+          ),
         ),
         GoRoute(
           path: '/dashboard/optimizations',
