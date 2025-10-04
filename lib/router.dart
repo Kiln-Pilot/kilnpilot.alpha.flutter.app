1c0555282f6ba1ea2420b580f49fa5c1ceec4b99
nailsonseat <aadarshv3@gmail.com>
2025-10-05 02:11:03 +0530

    update router

--- lib/router.dart
+++ lib/router.dart
@@ -3,14 +3,18 @@ import 'package:go_router/go_router.dart';
 import 'package:kilnpilot_alpha_flutter_app/blocs/chatbot/chatbot_bloc.dart';
 import 'package:kilnpilot_alpha_flutter_app/blocs/chatbot/chatbot_session_bloc.dart';
 import 'package:kilnpilot_alpha_flutter_app/repositories/cement_operations/chatbot_repository.dart';
+import 'package:kilnpilot_alpha_flutter_app/repositories/optimization/optimization_repository.dart';
 import 'package:kilnpilot_alpha_flutter_app/repositories/thermal_kpi/thermal_kpi_repository.dart';
 import 'package:kilnpilot_alpha_flutter_app/screens/dashboard/dashboard.dart';
 import 'package:kilnpilot_alpha_flutter_app/screens/dashboard/dashboard_shell.dart';
 import 'package:kilnpilot_alpha_flutter_app/screens/kiln_temperature/kiln_temperature_screen.dart';
+import 'package:kilnpilot_alpha_flutter_app/screens/optimization/optimization_detail_screen.dart';
 import 'package:kilnpilot_alpha_flutter_app/screens/optimization/optimization_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kilnpilot_alpha_flutter_app/blocs/thermal_kpi/thermal_kpi_bloc.dart';
 import 'package:kilnpilot_alpha_flutter_app/screens/chat/chatbot_screen.dart';
+
+import 'blocs/optimization/optimization_bloc.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -25,7 +29,10 @@ final GoRouter router = GoRouter(
   initialLocation: '/dashboard',
   routes: [
     ShellRoute(
-      builder: (context, state, child) => DashboardShell(child: child),
+      builder: (context, state, child) => BlocProvider(
+        create: (context) => OptimizationBloc(OptimizationRepository())..add(ListOptimizations()),
+        child: DashboardShell(child: child),
+      ),
       routes: [
         GoRoute(
           path: '/dashboard',
@@ -36,12 +43,8 @@ final GoRouter router = GoRouter(
           pageBuilder: (context, state) => MaterialPage(
             child: MultiBlocProvider(
               providers: [
-                BlocProvider(
-                  create: (_) => ChatbotBloc(ChatbotRepository()),
-                ),
-                BlocProvider(
-                  create: (_) => ChatbotSessionBloc(ChatbotRepository())
-                ),
+                BlocProvider(create: (_) => ChatbotBloc(ChatbotRepository())),
+                BlocProvider(create: (_) => ChatbotSessionBloc(ChatbotRepository())),
               ],
               child: ChatbotScreen(),
             ),
@@ -49,15 +52,21 @@ final GoRouter router = GoRouter(
         ),
         GoRoute(
           path: '/dashboard/optimizations',
-          pageBuilder: (context, state) => MaterialPage(child: OptimizationsScreen()),
+          pageBuilder: (context, state) => MaterialPage(child: const OptimizationsScreen()),
+          routes: [
+            GoRoute(
+              path: 'detail/:optimizationId',
+              pageBuilder: (context, state) {
+                final optimizationId = state.pathParameters['optimizationId']!;
+                return MaterialPage(child: OptimizationDetailScreen(optimizationId: optimizationId));
+              },
+            ),
+          ],
         ),
         GoRoute(
           path: '/dashboard/kiln-temperature',
           pageBuilder: (context, state) => MaterialPage(
-            child: BlocProvider(
-              create: (_) => ThermalKpiBloc(ThermalRepository()),
-              child: KilnTemperatureScreen(),
-            ),
+            child: BlocProvider(create: (_) => ThermalKpiBloc(ThermalRepository()), child: KilnTemperatureScreen()),
           ),
         ),
       ],
