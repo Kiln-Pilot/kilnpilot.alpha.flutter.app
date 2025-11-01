6365503b1406b0dc3e26cb6abcd7323919f41a20
nailsonseat <aadarshv3@gmail.com>
2025-11-01 08:31:30 +0530

    update settings

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppConstants {
-  static const String appName = "Kiln Pilot";
-  static const bool isSsl = false;
-  static const bool mentionPort = true;
-  static const String host = "localhost";
+  static const bool isDebug = true;
+  static const bool isSsl = isDebug ? false : true;
+  static const bool mentionPort = isDebug ? true : false;
+  static const String host = isDebug ? "localhost" : "kilnpilot-fastapi.onrender.com/";
   static const int port = 8000;
   static const String apiPath = "/api/v1";
   static const String baseUrl =
-      "${isSsl ? "https" : "http"}://$host:"
-      "${mentionPort ? port : ""}"
+      "${isSsl ? "https" : "http"}://$host"
+      "${mentionPort ? ":$port" : ""}"
       "$apiPath";
+  static const String appName = "Kiln Pilot";
   static const Color seedColor = Colors.deepPurple;
 }
